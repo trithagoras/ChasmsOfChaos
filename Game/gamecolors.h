7180b191ea14dcd4e1d99913e47843ba524899c3
corey <coreybatchler7@gmail.com>
2024-05-02 21:09:37 +1000

    add placeholder UI elements

--- Game/gamecolors.h
+++ Game/gamecolors.h
@@ -4,3 +4,4 @@
 
 const sf::Color TAN = sf::Color(0xD2B48CFF);
 const sf::Color DARKMAROON = sf::Color(0x472D3CFF);
+const sf::Color WOOD = sf::Color(0x633E2ECC);
--- Game/ui.cpp
+++ Game/ui.cpp
@@ -4,6 +4,7 @@
 #include "world.h"
 #include <format>
 #include "spritefactory.h"
+#include "gamecolors.h"
 
 void UI::initialize(const sf::RenderWindow& window) {
 	sf::View uiView;
@@ -17,10 +18,11 @@ UI& UI::get_instance() {
 	return instance;
 }
 
-sf::Text make_text(const std::string& stext) {
+sf::Text make_text(const std::string& stext, int x = 0, int y = 0) {
 	const auto& font = ContentProvider::get_instance().get_font("Consolas.ttf");
 
 	sf::Text text(stext, font, 24);
+	text.setPosition(sf::Vector2f(x, y));
 	text.setFillColor(sf::Color::White);
 	text.setStyle(sf::Text::Bold);
 	text.setOutlineColor(sf::Color::Black);
@@ -29,24 +31,69 @@ sf::Text make_text(const std::string& stext) {
 }
 
 void UI::draw_ui(sf::RenderWindow& window) {
-	auto oldView = sf::View(window.getView());
+	sf::View oldView(window.getView());
 
 	auto& world = World::get_instance();
 	auto& floor = world.get_current_floor();
 	const auto& player = floor.get_player();
-	auto& sprites = 
+	auto& sprites = SpriteFactory::get_instance();
+
+	auto [width, height] = uiView.getSize();
 
 	// set view to UI view before drawing
-	// ####################################################
-
 	window.setView(uiView);
-	// TODO: this should be changed to ladder icon with rest of UI
-	auto floorText = make_text(std::format("Floor: {}", world.get_floor_num()));
-	floorText.setPosition(10, 10);
-	window.draw(floorText);
+	// ####################################################
+	// bottom UI bar
+	sf::RectangleShape bar(sf::Vector2f(width, 64));
+	bar.setPosition(0, height - 64);
+	bar.setFillColor(WOOD);
+	window.draw(bar);
 
-	// health, AC, mana, EXP
-	auto& hearticon = 
+	// drawing bottom ui elements
+	int i = 32;
+
+	// floor
+	auto flooricon = sprites.create_sprite("ladder icon");
+	flooricon.setPosition(i, height - 48);
+	flooricon.setScale(2, 2);
+	window.draw(flooricon);
+	window.draw(make_text(std::format("{:02}", world.get_floor_num() + 1), i + 40, height - 48));
+
+	i += 128;
+
+	// health
+	auto hearticon = sprites.create_sprite("heart icon");
+	hearticon.setPosition(i, height - 48);
+	hearticon.setScale(2, 2);
+	window.draw(hearticon);
+	window.draw(make_text(std::format("{:02}/{:02}", 8, 15), i + 40, height - 48));
+
+	i += 128;
+
+	// mana
+	auto manaicon = sprites.create_sprite("mana icon");
+	manaicon.setPosition(i, height - 48);
+	manaicon.setScale(2, 2);
+	window.draw(manaicon);
+	window.draw(make_text(std::format("{:02}/{:02}", 15, 15), i + 40, height - 48));
+
+	i += 128;
+
+	// AC
+	auto acicon = sprites.create_sprite("shield icon");
+	acicon.setPosition(i, height - 48);
+	acicon.setScale(2, 2);
+	window.draw(acicon);
+	window.draw(make_text(std::format("{:02}", 11), i + 40, height - 48));
+
+	i += 128;
+
+	// EXP
+	auto expicon = sprites.create_sprite("exp icon");
+	expicon.setPosition(i, height - 48);
+	expicon.setScale(2, 2);
+	window.draw(expicon);
+	window.draw(make_text(std::format("{:04}/{:04}", 16, 64), i + 40, height - 48));
 
 	// ####################################################
 	// reset view to game view after UI drawing is done
